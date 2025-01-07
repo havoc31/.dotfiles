3c6a83d1e22471a3d91660f1ae4dce496cffff48
Luke Storm <lukestorm@tuta.io>
2025-01-07 15:18:28 +0200

    Updated nvim lua config

--- private_dot_config/nvim/lua/plugins/nvim-treesitter.lua
+++ private_dot_config/nvim/lua/plugins/nvim-treesitter.lua
@@ -7,7 +7,7 @@ return {
 
       config.setup({
         auto_install = true,
-        ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
+        ensure_installed = { "c", "cpp", "bash", "lua", "python", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
         highlight = { enable = true },
         -- indent = { enable = true },
       })
