7ceb8e9d835c1ecc5d68e35b19f504c42d821b14
Luke Storm <lukestorm@tuta.io>
2025-01-08 16:21:58 +0200

    Updated modus theme config for nvim

--- private_dot_config/nvim/lua/plugins/modus.lua
+++ private_dot_config/nvim/lua/plugins/modus.lua
@@ -3,6 +3,18 @@ return {
     lazy = false,
     priority = 1000,
     config = function()
-        vim.cmd.colorscheme("modus_vivendi")
+        local modus = require("modus-themes")
+        modus.setup({
+            style = "modus_vivendi",    
+
+            on_colors = function (colors)
+                colors.bg_hl_line = colors.bg_active
+            end,
+
+            on_highlights = function (highlights, color)
+                highlights.NvimTreeNormal.bg = color.black
+            end,
+        })
+        vim.cmd.colorscheme("modus")
     end
 }
